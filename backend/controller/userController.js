ff14ab5d645b65e4a4c093deb59adde50d5779fb
saadakram <saadakram288@gmail.com>
2024-07-25 16:55:02 +0500

    Connected to MongoDB

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,22 @@
-const authUser = (req, res) => {
+import asynchandler from "express-async-handler";
+
+const authUser = asynchandler(async (req, res) => {
   res.status(200).json({ message: "auth user" });
-};
-export { authUser };
+});
+
+const registereUser = asynchandler(async (req, res) => {
+  res.status(200).json({ message: "Register user" });
+});
+
+const logoutUser = asynchandler(async (req, res) => {
+  res.status(200).json({ message: "Logout user" });
+});
+
+const getUserProfile = asynchandler(async (req, res) => {
+  res.status(200).json({ message: "Get user" });
+});
+
+const updateUserProfile = asynchandler(async (req, res) => {
+  res.status(200).json({ message: "update user" });
+});
+export { authUser, registereUser, logoutUser, getUserProfile, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,17 @@
 import express from "express";
-import { authUser } from "../controller/userController.js";
+import {
+  authUser,
+  registereUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controller/userController.js";
 const router = express.Router();
 
-router.post("/auth", authUser)
+router.post("/", registereUser);
+router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.get("/getUser", getUserProfile);
+router.put("/updateUser", updateUserProfile);
 
 export default router;
