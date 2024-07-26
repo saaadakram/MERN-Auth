61dd45a41eb8980187109906df59235aa629754e
saadakram <saadakram288@gmail.com>
2024-07-26 06:41:16 +0500

    Created the token

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,11 +1,34 @@
 import asynchandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 const authUser = asynchandler(async (req, res) => {
   res.status(200).json({ message: "auth user" });
 });
 
 const registereUser = asynchandler(async (req, res) => {
-  res.status(200).json({ message: "Register user" });
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email: email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already registered");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    generateToken(res, user._id)
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User");
+  }
 });
 
 const logoutUser = asynchandler(async (req, res) => {
@@ -19,4 +42,10 @@ const getUserProfile = asynchandler(async (req, res) => {
 const updateUserProfile = asynchandler(async (req, res) => {
   res.status(200).json({ message: "update user" });
 });
-export { authUser, registereUser, logoutUser, getUserProfile, updateUserProfile };
+export {
+  authUser,
+  registereUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
