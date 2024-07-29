cfffd181c9c0af12409b084192138d5d7a9baca2
saadakram <saadakram288@gmail.com>
2024-07-29 09:47:32 +0500

    Completed the project Alhamdulilah

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import Header from './componets/Header'
 import { Outlet } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+import {ToastContainer} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 import React from 'react'
 
 const App = () => {
   return (
     <>
     <Header/>
+    <ToastContainer/>
     <Container className='my-2'>
     <Outlet/>
     </Container>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,30 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from "react-router-dom";
-import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
-import HomeScreen from "./screen/HomeScreen.jsx";
-import LoginScreen from "./screen/LoginScreen.jsx";
-import RegisterScreen from "./screen/RegisterScreen.jsx";
+} from 'react-router-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './store';
+import { Provider } from 'react-redux';
+import HomeScreen from "./screen/HomeScreen.jsx"
+import LoginScreen from "./screen/LoginScreen.jsx"
+import RegisterScreen from "./screen/RegisterScreen.jsx"
+import ProfileScreen from "./screen/ProfileScreen.jsx"
+import PrivateRoute from "./componets/PrivateRoute.jsx"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index = {true} path = "/" element= {<HomeScreen />}/>
-    <Route path = "/login" element= {<LoginScreen />}/>
-    <Route path = "/register" element= {<RegisterScreen />}/>
-    
-  </Route>)
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
+    </Route>
+  )
 );
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
